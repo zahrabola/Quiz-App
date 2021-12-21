1993bcc2f33337dee3618d49e5849495ec1d0879
zahrabola <95353397+zahrabola@users.noreply.github.com>
2021-12-21 11:21:09 +0000

    Update main.js
    add const question

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 //https://opentdb.com/api.php?amount=20&category=11&type=multiple
 
+const _question = document.getElementById("question");
+const _question = document.querySelector(".quiz-options");
+
+
 async function loadQuestion() {
   const APIUrl =
     "https://opentdb.com/api.php?amount=20&category=11&type=multiple";
